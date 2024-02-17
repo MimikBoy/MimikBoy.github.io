05765761937d6c64ce7e874ca72e45c6a63e704e
Pierluigi Gatt <68955502+MimikBoy@users.noreply.github.com>
2024-02-17 10:37:58 +0100

    Added NavBar

---
+++ script.js
@@ -0,0 +1,10 @@
+function navCreate(){
+    fetch('reusables/nav.html')
+    .then(res => res.text())
+    .then(text => {
+    let oldelem = document.querySelector("#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+    })
+}
