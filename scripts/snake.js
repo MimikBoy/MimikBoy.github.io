a9c88774beb70a5a16ee0269d6d18325e2438451
Pierluigi Gatt <68955502+MimikBoy@users.noreply.github.com>
2024-02-22 15:34:41 +0100

    touch?

--- scripts/snake.js
+++ scripts/snake.js
@@ -3,17 +3,66 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 var divBottom = canvas.getBoundingClientRect().top;
 
-//change canvas dimentions
-// ctx.canvas.width = window.innerWidth - 25;
-// ctx.canvas.height = window.innerHeight - divBottom;
-console.log(canvas.width);
 // Set initial game state
 let snake = [{ x: 10, y: 10 }];
 let food = { x: 15, y: 15 };
 let direction = "right";
 let gameSpeed = 100;
 let gameOver = false;
+let mobile = false;
+let xDown = null;
+let yDown = null;
 
+//check for mobile user
+function isMobile() {
+    const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+
+    if (regex.test(navigator.userAgent)) {
+        mobile = true;
+    } 
+
+    return;
+}
+
+function getTouches(evt){
+    return evt.touches || evt.originalEvent.touches;
+}
+
+// function handleTouchStart(evt){
+//     const firstTouch = getTouches(evt)[0];
+//     xDown = firstTouch.clientX;
+//     yDown = firstTouch.clientY;
+// }
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            if (direction !== "left") direction = "right"; 
+        } else {
+            if (direction !== "right") direction = "left";
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            if (direction !== "up") direction = "down";
+        } else { 
+            if (direction !== "down") direction = "up";
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+}
+  
 // Main game loop
 function gameLoop() {
     if (gameOver) return;
@@ -101,7 +150,14 @@ function render() {
     });
 }
 
-// Handle keyboard input
+
+isMobile();
+
+if(mobile){
+    // Handle touch input
+    document.addEventListener('touchmove', handleTouchMove, false);
+} else {
+    // Handle keyboard input
 document.addEventListener("keydown", event => {
     switch (event.key) {
         case "ArrowUp":
@@ -118,15 +174,7 @@ document.addEventListener("keydown", event => {
             break;
     }
 });
-
-//Handle resizing
-// window.addEventListener("resize", event => {
-//     divBottom = canvas.getBoundingClientRect().top;
-
-//     //change canvas dimentions
-//     ctx.canvas.width = window.innerWidth - 25;
-//     ctx.canvas.height = window.innerHeight - divBottom - 10;
-// })
-
+}
 // Start the game loop
+
 gameLoop();
